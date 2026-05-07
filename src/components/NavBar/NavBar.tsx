886312d28f416e0439cf3f2c9639e4cf3920f164
Alex Martin <sharkfinpro@hotmail.com>
2026-05-07 10:53:23 -0700

    Add NavBar styles and apply CSS module
    Introduce NavBar.module.scss with header, container, and logo styles (sticky header, layout, spacing, and theme variables). Update NavBar.tsx to import the CSS module and apply header, container, and logo classes to the markup. Add a --max-width CSS custom property to global.scss to constrain the container width.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import styles from "./NavBar.module.scss";
 
 export default function NavBar() {
   return (
-    <header>
-      <Link href="/">Tayler Carney</Link>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Link href="/" className={styles.logo}>Tayler Carney</Link>
+      </div>
     </header>
   );
 }
