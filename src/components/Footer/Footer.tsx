36d91d2adb16487d614a0f0daa673d3dbd8cdd56
Alex Martin <sharkfinpro@hotmail.com>
2026-05-07 10:27:22 -0700

    Add Footer styles and theme root
    Introduce Footer.module.scss and apply it in Footer.tsx to style the footer wrapper (padding, background-color, inverse text color). Also update global.scss to emit the light theme CSS variables at :root so theme variables are available globally.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.scss";
+
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.wrapper}>
       <p>© 2026 Tayler Carney.</p>
     </footer>
   );
